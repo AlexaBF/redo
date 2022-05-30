c059e2f3cd6ee95915cd7db1c95d6282c776cd6b
KarlaTrejo <A01422942@itesm.mx>
2022-05-30 13:23:03 -0500

    Beneficiaries update with a .csv file (Endpoint)

--- routes/beneficiaryDocs.js
+++ routes/beneficiaryDocs.js
@@ -1,10 +1,64 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+
 const router = express.Router()
-const {connection} = require("../config/mysql.js")
+const {connection} = require("../config/mysql.js");
 const path = '/beneficiaryDocs'
 
+const app = express.Router();
+
+app.use(fileUpload()); //Recibe y procesa
+
+const csv = require('csv-parser');
+const fs = require('fs');
+
+
+app.post(path, (req, res)=>{
+    if(!req.files || Object.keys(req.files).length === 0){ //Si no existe el atributo files en el req o si el archivo se encuentra vacío
+        return res.status(400).send('No se enviaron archivos');
+    }
+    let sampleFile = req.files.archivo;
+    console.log(req.files);
+    sampleFile.mv(`./files/${sampleFile.name}`,err => {
+        if(err) {
+            return res.status(400).send({message: err})
+        }
+    })
+    //name, data, size, mimetype
+    let count = -1;
+    fs.createReadStream(`./files/${sampleFile.name}`)
+    .pipe(csv())
+    .on('data', (row) => {
+        count ++;
+        console.log(row)
+        console.log(count)
+        if (Object.keys(row).length===11) {
+            connection.query("CALL `REDO_MAKMA`.`beneficiaryDocs`(?,?,?,?,?,?,?,?,?,?,?,?);"
+                , [row['FOLIO FAMILIAR'], row['NOMBRE TITULAR'], row.COLONIA, row.DIA, row['F.REGISTRO'], row['F.VENCIMIENTO'],
+                    row.ESTADO, row.BECA, row.FRECUENCIA, row.TELEFONO, row.SUCURSAL, count], (err, result, fields) => {
+                if (err) {
+                    console.log(err)
+                    return res.status(500).send({
+                        done: false
+                    })
+                } else {
+                    console.log(row['FOLIO FAMILIAR'], row['NOMBRE TITULAR'], row.COLONIA, row.DIA, row['F.REGISTRO'], row['F.VENCIMIENTO'],
+                        row.ESTADO, row.BECA, row.FRECUENCIA, row.TELEFONO, row.SUCURSAL, count);
+                    console.log( result );
+                }
+            });
+        }
+    })
+    .on('end', () => {
+        console.log('CSV file successfully processed');
+        return res.send({done: true});
+    });
+});
+
+
+
 //specific documents of the beneficiaries are added/modified
-router.put(path, (req, res)=>{
+app.put(path, (req, res)=>{
     const { id,badge,socioeconomicStudy } = req.body
     console.log(req.body)
     connection.query("CALL `REDO_MAKMA`.`updateBeneficiaryDocs`(?,?,?);"
@@ -20,6 +74,6 @@ router.put(path, (req, res)=>{
     })
 })
 
-
-module.exports = router;
+module.exports = app;
+//module.exports = router;
 
