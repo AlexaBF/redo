ba363698cdebd86274e179b9fb153e6b0aa5a46c
KarlaTrejo <A01422942@itesm.mx>
2022-06-02 08:50:51 -0500

    Endpoints for the update and get of badges

--- routes/beneficiaryDocs.js
+++ routes/beneficiaryDocs.js
@@ -12,7 +12,7 @@ app.use(fileUpload()); //Recibe y procesa
 const csv = require('csv-parser');
 const fs = require('fs');
 
-
+//NUEVO-Subida masiva con archivo de beneficiarios.
 app.post(path, (req, res)=>{
     if(!req.files || Object.keys(req.files).length === 0){ //Si no existe el atributo files en el req o si el archivo se encuentra vacío
         return res.status(400).send('No se enviaron archivos');
@@ -59,10 +59,15 @@ app.post(path, (req, res)=>{
 
 //specific documents of the beneficiaries are added/modified
 app.put(path, (req, res)=>{
-    const { id,badge,socioeconomicStudy } = req.body
+    if(!req.files || Object.keys(req.files).length === 0){ //Si no existe el atributo files en el req o si el archivo se encuentra vacío
+        return res.status(400).send('No se enviaron archivos');
+    }
+    let sampleFile = req.files.archivo;
+    console.log(sampleFile);
     console.log(req.body)
-    connection.query("CALL `REDO_MAKMA`.`updateBeneficiaryDocs`(?,?,?);"
-    ,[id,badge,socioeconomicStudy], (err, result, fields) =>{
+    //name, data, size, mimetype
+    connection.query("CALL `REDO_MAKMA`.`updateBeneficiaryDocs`(?,?,?,?,?);"
+    ,[req.body.idBeneficiary, sampleFile.name, sampleFile.data, sampleFile.size, sampleFile.mimetype], (err, result, fields) =>{
         if(err){
             console.log(err)
             res.status(500).send({
@@ -72,6 +77,29 @@ app.put(path, (req, res)=>{
             res.json({done:true});
         }
     })
+})
+
+
+//NUEVO-obtención de archivos de todos los beneficiarios
+app.get(path, (req, res)=>{
+    console.log(req.body)
+    const { IdBranch } = req.token
+    //name, data, size, mimetype
+    connection.query("CALL `REDO_MAKMA`.`readBeneficiaryDocs`(?);"
+        ,[IdBranch], (err, result, fields) =>{
+            if(err){
+                console.log(err)
+                res.status(500).send({
+                    done:false
+                })
+            }else{
+                //No es res.json
+                res.setHeader('Content-Disposition', `attachment; filename="${results[0].name}"`); //Forzar la descarga del archivo  y necesita un nombre (results[0].name)
+                res.setHeader('Content-Type', results[0].mimetype) //¿De qué tipo es el conteniodo del archivo?  Porque cada contenido que se sube a internet tiene varios nombres application/
+                res.send(results[0].data); //Escribes bytes en el response
+                //De esta forma el response es un archivo
+            }
+        })
 })
 
 module.exports = app;
