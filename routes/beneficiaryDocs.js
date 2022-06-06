b12f8f8d4b3943f98eec4b658c2fac948890f961
KarlaTrejo <A01422942@itesm.mx>
2022-06-06 13:09:15 -0500

    Update in beneficiary Docs

--- routes/beneficiaryDocs.js
+++ routes/beneficiaryDocs.js
@@ -3,7 +3,7 @@ const fileUpload = require('express-fileupload');
 
 const router = express.Router()
 const {connection} = require("../config/mysql.js");
-const path = '/beneficiaryDoc'
+const path = '/beneficiaryDocs'
 
 const app = express.Router();
 
@@ -17,8 +17,8 @@ app.post(path, (req, res)=>{
     if(!req.files || Object.keys(req.files).length === 0){ //Si no existe el atributo files en el req o si el archivo se encuentra vacío
         return res.status(400).send('No se enviaron archivos');
     }
-    let sampleFile = req.files.BeneficiariesData;
-    console.log(req.files);
+    let sampleFile = req.files.file;
+    // console.log(req);
     sampleFile.mv(`./files/${sampleFile.name}`,err => {
         if(err) {
             return res.status(400).send({message: err})
@@ -27,45 +27,45 @@ app.post(path, (req, res)=>{
     //name, data, size, mimetype
     let count = -1;
     fs.createReadStream(`./files/${sampleFile.name}`)
-    .pipe(csv())
-    .on('data', (row) => {
-        count ++;
-        console.log(row)
-        console.log(count)
-        //const {FOLIO,NOMBRE,COLONIA,DIA,FREGISTRO,FVENCIMIENTO,ESTADO,BECA,FRECUENCIA,TELEFONO,SUCURSAL} = row
-        if (Object.keys(row).length===11) {
-            connection.query("CALL `REDO_MAKMA`.`beneficiaryDocs`(?,?,?,?,?,?,?,?,?,?,?,?);"
-                , [row['FOLIO FAMILIAR'], row['NOMBRE TITULAR'], row.COLONIA, row.DIA, row['F.REGISTRO'], row['F.VENCIMIENTO'],
-                    row.ESTADO, row.BECA, row.FRECUENCIA, row.TELEFONO, row.SUCURSAL, count], (err, result, fields) => {
-                if (err) {
-                    console.log(err)
-                    return res.status(500).send({
-                        done: false
-                    })
-                } else {
-                    console.log(row['FOLIO FAMILIAR'], row['NOMBRE TITULAR'], row.COLONIA, row.DIA, row['F.REGISTRO'], row['F.VENCIMIENTO'],
-                        row.ESTADO, row.BECA, row.FRECUENCIA, row.TELEFONO, row.SUCURSAL, count);
-                    console.log( result );
-                }
-            });
-        }
-    })
-    .on('end', () => {
-        console.log('CSV file successfully processed');
-        return res.send({done: true});
-    });
+        .pipe(csv())
+        .on('data', (row) => {
+            count ++;
+            console.log(row)
+            console.log(count)
+            if (Object.keys(row).length===11) {
+                connection.query("CALL `REDO_MAKMA`.`beneficiaryDocs`(?,?,?,?,?,?,?,?,?,?,?,?);"
+                    , [row['FOLIO FAMILIAR'], row['NOMBRE TITULAR'], row.COLONIA, row.DIA, row['F.REGISTRO'], row['F.VENCIMIENTO'],
+                        row.ESTADO, row.BECA, row.FRECUENCIA, row.TELEFONO, row.SUCURSAL, count], (err, result, fields) => {
+                        if (err) {
+                            console.log(err)
+                            return res.status(500).send({
+                                done: false
+                            })
+                        } else {
+                            console.log(row['FOLIO FAMILIAR'], row['NOMBRE TITULAR'], row.COLONIA, row.DIA, row['F.REGISTRO'], row['F.VENCIMIENTO'],
+                                row.ESTADO, row.BECA, row.FRECUENCIA, row.TELEFONO, row.SUCURSAL, count);
+                            console.log( result );
+                        }
+                    });
+            }
+        })
+        .on('end', () => {
+            console.log('CSV file successfully processed');
+            return res.send({done: true});
+        });
 });
 
 
 
 //specific documents of the beneficiaries are added/modified
 app.put(path, (req, res)=>{
+    //console.log(req);
     if(!req.files || Object.keys(req.files).length === 0){ //Si no existe el atributo files en el req o si el archivo se encuentra vacío
         return res.status(400).send('No se enviaron archivos');
     }
-    let sampleFile = req.files.Badge;
+    let sampleFile = req.files.file;
     console.log(sampleFile);
-    console.log(req.body)
+    //console.log(req.body)
     const { IdBeneficiary } = req.body
     //name, data, size, mimetype
     connection.query("CALL `REDO_MAKMA`.`updateBeneficiaryDocs`(?,?,?,?,?);"
