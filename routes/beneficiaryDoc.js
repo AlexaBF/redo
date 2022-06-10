f6622a9099008d02233e67e58faf3693e78636c3
KarlaTrejo <A01422942@itesm.mx>
2022-06-10 11:14:50 -0500

    update of get endpoint in beneficiaryDoc.js

--- routes/beneficiaryDoc.js
+++ routes/beneficiaryDoc.js
@@ -11,7 +11,7 @@ app.use(fileUpload()); //Recibe y procesa
 
 //NUEVO-obtención de archivos de uno de los beneficiarios
 app.get(path, (req, res)=>{
-    //console.log(req.body)
+    console.log(req.body)
     const IdBeneficiary = req.params.IdBeneficiary;
     //const { IdBeneficiary } = req.body
     //name, data, size, mimetype
@@ -23,10 +23,13 @@ app.get(path, (req, res)=>{
                     done:false
                 })
             }else{
+                const mydata = Object.values(JSON.parse(JSON.stringify(result[0])));
+                console.log(mydata)
+                console.log('\n\n',mydata[0].Nombre)
                 //No es res.json
-                res.setHeader('Content-Disposition', `attachment; filename="${result[0].Nombre}"`); //Forzar la descarga del archivo  y necesita un nombre (results[0].name)
-                res.setHeader('Content-Type', result[0].Mimetype) //¿De qué tipo es el conteniodo del archivo?  Porque cada contenido que se sube a internet tiene varios nombres application/
-                res.send(result[0].Data); //Escribes bytes en el response
+                res.setHeader('Content-Disposition', `attachment; filename="${mydata[0].Nombre}"`); //Forzar la descarga del archivo  y necesita un nombre (results[0].name)
+                res.setHeader('Content-Type', mydata[0].Mimetype) //¿De qué tipo es el conteniodo del archivo?  Porque cada contenido que se sube a internet tiene varios nombres application/
+                res.send(mydata[0].Data); //Escribes bytes en el response
                 //De esta forma el response es un archivo
             }
         })
